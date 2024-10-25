6e5e5dc8f508f964b530f00acee4ae6832312ce3
DGLutsky <dglutsky@greenatom.ru>
2024-10-25 20:15:29 +1200

    первый коммит

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import * as path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import rollupReplace from "@rollup/plugin-replace";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    //@ts-ignore
+    rollupReplace({
+      preventAssignment: true,
+      values: {
+        "process.env.NODE_ENV": JSON.stringify("development"),
+      },
+    }),
+    react(),
+  ],
+});
